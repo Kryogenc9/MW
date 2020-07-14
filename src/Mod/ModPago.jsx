12cc2e24c37bba25a4044270bce5586403079fcc
Leandro <pack1409@gmail.com>
2020-07-13 22:21:51 -0400

    Mod bugged pero funciona2

--- src/Mod/ModPago.jsx
+++ src/Mod/ModPago.jsx
@@ -1,10 +1,9 @@
-import React,{useState} from "react";
-
+import React, {useState} from "react";
 import vta from "../CSS/VentasCss.module.css"
-import {Modal, Button} from "react-bootstrap"
+import {Modal, Button, ModalBody, ModalDialog, ModalFooter, ModalTitle} from "react-bootstrap"
+import mod from '../CSS/Modal.css'
+import ModalHeader from "react-bootstrap/ModalHeader";
 
-//import 'bootstrap/dist/js/bootstrap.bundle.min';
-import mod from "../CSS/Modal.css"
 
 /*export default class ModPago extends React.Component{
     constructor(props) {
@@ -17,11 +16,11 @@ import mod from "../CSS/Modal.css"
 
 export default function ModPago() {
     const [show, setShow] = useState(false)
-    const [pay, payLoad]= useState(false)
+    const [pay, payLoad] = useState(false)
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-    const pagoEfectivo= () => payLoad(true);
+    const pagoEfectivo = () => payLoad(true);
 
     return (
         <>
@@ -34,32 +33,38 @@ export default function ModPago() {
                 onHide={handleClose}
                 backdrop="static"
                 keyboard={false}
+                size="xl"
+                dialogClassName={mod.customModal}
+
 
             >
-                <Modal.Header closeButton /*bsPrefix={`${mod.Header}modal-tittle`}*/>
-                    <Modal.Title>Pagos MW</Modal.Title>
-                </Modal.Header>
-                <Modal.Body bsPrefix={`${mod.Body} modal-body`}>
-                    <form className={mod.form}>
-                        <label>Monto a Pagar:</label> <label>$999.999</label>
-                        <input type="number" name="Pago" placeholder={"999.999"}/>
-                        <div>
-                            <input type="radio" id="efectivo" name="pago" value="Efectivo" />
-                            <label for="efectivo">Efectivo</label>
-                            <input type="radio" id="tarjeta" name="pago" value="Tarjeta"/>
-                            <label For="tarjeta">Tarjeta</label>
-                        </div>
 
-                    </form>
+                    <ModalHeader closeButton="true" >
+                        <ModalTitle bsPrefix={`${mod.Header} modal-tittle`}>Pagos MW</ModalTitle>
+                    </ModalHeader>
+                    <ModalBody bsPrefix={`${mod.Body} modal-body`}>
+
+                        <form className={mod.form}>
+                            <label>Monto a Pagar:</label> <label>$999.999</label>
+                            <input type="number" name="Pago" placeholder={"999.999"}/>
+                            <div>
+                                <input type="radio" id="efectivo" name="pago" value="Efectivo"/>
+                                <label for="efectivo" >Efectivo</label>
+                                <input type="radio" id="tarjeta" name="pago" value="Tarjeta"/>
+                                <label For="tarjeta">Tarjeta</label>
+                            </div>
+
+                        </form>
 
 
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Cancelar
-                    </Button>
-                    <Button variant="primary" onClick={handleClose}>Pagar</Button>
-                </Modal.Footer>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Cancelar
+                        </Button>
+                        <Button variant="primary" onClick={handleClose}>Pagar</Button>
+                    </ModalFooter>
+
             </Modal>
         </>
     );
